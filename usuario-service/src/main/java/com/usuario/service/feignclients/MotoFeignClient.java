a976974b6fbe389cc8af25929becfed13c6b353b
ChristianRaulRamirez <cramirezchristian77@gmail.com>
2022-04-05 22:19:55 -0500

    gateway

--- usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.usuario.service.modelos.Moto;
 
-@FeignClient(name = "moto-service",url = "http://localhost:8003")
+@FeignClient(name = "moto-service")
 @RequestMapping("/moto")
 public interface MotoFeignClient {
 
