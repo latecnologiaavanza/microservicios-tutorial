9b855d002fecf2ecdad40b6ee050ba8b295a776b
ChristianRaulRamirez <cramirezchristian77@gmail.com>
2022-04-05 21:32:28 -0500

    gateway

---
+++ gateway-service/src/test/java/com/gateway/service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
